48ba59443cc51aef25cc42e1eee624741a3f3f71
Cem Ozgur <cemozgur9@hotmail.com>
2025-12-30 17:35:09 +0300

    RequestVote

--- internal/raft/server.go
+++ internal/raft/server.go
@@ -1,33 +1,63 @@
 package raft
 
 import (
+	"math/rand"
 	"raft/internal/maths"
 	"raft/internal/slices"
 )
 
-type State struct {
-	currentTerm int
-	votedFor    string
+type Volatile struct {
 }
 
+type State int
+
+const (
+	FOLLOWER State = iota
+	CANDIDATE
+	LEADER
+)
+
 type Server struct {
-	state       *State
+	state       State
+	currentTerm int
+	votedFor    int
 	commitIndex int
 	lastApplied int
 	nextIndex   []int
 	matchIndex  []int
 	logStore    LogStore
+	timeoutMs   int
+}
+
+func NewServer(
+	logStore LogStore,
+	term int,
+	timeoutMin int,
+	timeoutMax int,
+) Server {
+	server := Server{
+		state:       FOLLOWER,
+		currentTerm: term,
+		votedFor:    -1,
+		commitIndex: -1,
+		lastApplied: -1,
+		nextIndex:   make([]int, 5),
+		matchIndex:  make([]int, 5),
+		logStore:    logStore,
+		timeoutMs:   rand.Intn(timeoutMax-timeoutMin+1) + timeoutMin,
+	}
+
+	return server
 }
 
 func (server *Server) AppendEntries(
 	leaderTerm int,
-	leaderId int,
 	prevLogIndex int,
 	entries []Log,
 	leaderCommit int,
 ) (term int, success bool) {
-	if term < server.state.currentTerm {
-		return server.state.currentTerm, false
+	if term < server.currentTerm {
+		return server.currentTerm, false
 	}
 
 	log, err := server.logStore.Get(prevLogIndex)
@@ -36,19 +66,23 @@ func (server *Server) AppendEntries(
 	}
 
 	if log.term != leaderTerm {
-		return server.state.currentTerm, false
+		return server.currentTerm, false
 	}
 
 	logs, _ := server.logStore.GetAll()
 
 	var i int
-	entriesToAdd := make([]*Log, 0)
 	for ; i < len(logs); i++ {
 		for _, entry := range entries {
 			if entry.IsConflictingWith(&logs[i]) {
 				break
 			}
+		}
+	}
 
+	entriesToAdd := make([]*Log, 0)
+	for ; i < len(logs); i++ {
+		for _, entry := range entries {
 			entriesToAdd = append(entriesToAdd, &entry)
 			if entry.IsEqual(&logs[i]) {
 				entriesToAdd = slices.RemoveByValue(entriesToAdd, &entry)
@@ -74,5 +108,28 @@ func (server *Server) AppendEntries(
 		server.commitIndex = maths.Min(maxIndex, leaderCommit)
 	}
 
-	return server.state.currentTerm, true
+	return server.currentTerm, true
+}
+
+func (server *Server) RequestVote(
+	term int,
+	candidateId int,
+	lastLogIndex int,
+	lastLogTerm int,
+) (currentTerm int, voteGranted bool) {
+	if term < server.currentTerm {
+		return server.currentTerm, false
+	}
+
+	if server.votedFor != -1 && server.votedFor != candidateId {
+		return server.currentTerm, false
+	}
+
+	lastIndex, err := server.logStore.GetLastIndex()
+	if err != nil {
+		panic(err)
+	}
+
+	server.state = FOLLOWER
+	return server.currentTerm, server.currentTerm <= lastLogTerm && lastIndex <= lastLogIndex
 }
